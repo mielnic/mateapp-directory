253246dec0c6b21d6e3d713ce55a357684ee54f9
Paulo Mielnichuk <paulo@mielnichuk.com.ar>
2023-08-23 19:45:10 -0300

    Faker createdata command

---
+++ MateAppDirectory/directory/management/commands/createdata.py
@@ -0,0 +1,49 @@
+import random
+from django.core.management.base import BaseCommand
+from faker import Faker
+from directory.models import Address, Company, Person
+
+alid = Address.objects.last().id
+clid = Company.objects.last().id
+plid = Person.objects.last().id
+
+class Command(BaseCommand):
+    help = "Command Information"
+
+    def handle(self, *args, **kwargs):
+        fake = Faker()
+        
+        for _ in range(15):
+            street = fake.street_address()
+            city = fake.city()
+            state = fake.state()
+            country = fake.country()
+            postalCode = fake.postcode()
+            Address.objects.create(street=street, state=state, country=country, city=city, postalCode=postalCode)
+
+        for _ in range(15):
+            companyName = fake.company()
+            tax_id = fake.ein()
+            website = fake.domain_name()
+            companyPhone = fake.basic_phone_number()
+            companyNotes = fake.paragraph(nb_sentences=2, variable_nb_sentences=True)
+            aid = alid + random.randint(1,15)
+            address = Address.objects.get(id=aid)
+            Company.objects.create(companyName=companyName, tax_id=tax_id, website=website, companyPhone=companyPhone, companyNotes=companyNotes, address=address)
+
+        for _ in range(50):
+            lastName = fake.last_name()
+            firstName = fake.first_name()
+            celphone = fake.basic_phone_number()
+            workphone = fake.basic_phone_number()
+            email = fake.ascii_company_email()
+            position = fake.job()
+            notes = fake.paragraph(nb_sentences=2, variable_nb_sentences=True)
+            aid = alid + random.randint(1,15)
+            cid = clid + random.randint(1,15)
+            address = Address.objects.get(id=aid)
+            company = Company.objects.get(id=cid)
+            Person.objects.create(lastName=lastName, firstName=firstName, celphone=celphone, workphone=workphone, email=email, position=position, notes=notes, address=address, company=company)
+
+        print('SUCCESS!')
+
