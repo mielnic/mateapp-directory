255b9985ed44ba1bec3eed10c78bef0c4abe81c7
Paulo Mielnichuk <paulo@mielnichuk.com.ar>
2023-09-22 22:40:27 -0300

    Various fixes

--- MateAppDirectory/users/functions.py
+++ MateAppDirectory/users/functions.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 
 def checkUserActivationLimit(muser):
+    if int(settings.USER_LIMIT) == 0:
+        return True
     activeUsers = get_user_model().objects.filter(is_active=True).exclude(is_superuser=True).count()
     if activeUsers >= int(settings.USER_LIMIT) and not muser.is_active:
         return False
@@ -9,6 +11,8 @@ def checkUserActivationLimit(muser):
         return True
 
 def checkUserCreationLimit():
+    if int(settings.USER_LIMIT) == 0:
+        return True
     activeUsers = get_user_model().objects.filter(is_active=True).exclude(is_superuser=True).count()
     if activeUsers >= int(settings.USER_LIMIT):
         return False
